61522507465055901126f395f77c0e27d6e6dd56
SeyedNamiModarressi <s.namimodarressi@gmail.com>
2021-02-02 19:22:29 +0330

    login request added

---
+++ src/LoginRequest.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.net.Socket;
+/**
+ *  -- Login Request --
+ *
+ * in this class we send login request code and data to server
+ *
+ * project : Plants Vs Zombies
+ * AP Final Project
+ *
+ * ###############################
+ * @author Seyed Nami Modarressi #
+ * @author Mahdi Rahmani         #
+ * @since 2020                   #
+ * @version 1.0                  #
+ * ###############################
+ *
+ *
+ */
+public class LoginRequest {
+
+    private Socket socket;
+    private BufferedReader read;
+    private PrintWriter output;
+
+    public int start(String username , String password) throws IOException {
+        socket = new Socket("127.0.0.1", 5050);
+        output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        read = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try {
+            return login(username,password);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return -1;
+        }
+        finally {
+            close();
+        }
+    }
+
+    public int login(String username , String password) throws IOException {
+        output.println("0");
+        output.println(username);
+        output.println(password);
+        output.flush();
+        String response = read.readLine();
+        return Integer.parseInt(response);
+    }
+
+
+    public void close() throws IOException {
+        socket.close();
+        read.close();
+        output.close();
+    }
+
+}
